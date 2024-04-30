c55b64c9058b71b71354ea2edd537dcc7ca86e32
Sam Johnston <samj@samj.net>
2024-04-30 00:50:23 -0700

    shorten hero

--- src/components/BackgroundPattern/index.tsx
+++ src/components/BackgroundPattern/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BackgroundPattern() {
   return (
-    <div className="absolute top-0 left-0 right-0 h-[500px] -z-10" aria-hidden>
+    <div className="absolute top-0 left-0 right-0 h-[300px] -z-10" aria-hidden>
       <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent to-gray-50 dark:to-blue-600" />
       <div className={`bg-background-pattern w-full h-full opacity-10`} />
     </div>
