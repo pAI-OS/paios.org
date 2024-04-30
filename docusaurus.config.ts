2a134bc25f64475071129a4d2b9e3c0a3415d083
Sam Johnston <samj@samj.net>
2024-04-30 01:16:34 -0700

    configure github org & repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,10 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'pAI-OS', // Usually your GitHub org/user name.
+  projectName: 'paios.org', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
