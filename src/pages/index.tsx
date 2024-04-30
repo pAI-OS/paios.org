8150dc5e13edab5d65aa420042546df8a6e9d665
Sam Johnston <samj@samj.net>
2024-04-29 19:40:54 -0700

    migrate index.tsx to tailwind from clsx/modules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,21 @@
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
-import styles from './index.module.css';
-
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
+    <header className="bg-blue-500">
+      <div className="container mx-auto text-center py-24">
+        <Heading as="h1" className="text-4xl font-bold text-white">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <p className="text-xl py-6 text-white">{siteConfig.tagline}</p>
+        <div className="py-10">
           <Link
-            className="button button--secondary button--lg"
+            className="bg-white rounded-md text-gray-500 px-4 py-2"
             to="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
@@ -32,8 +29,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Personal AI Operating System`}
+      description={`Personal Artificial Intelligence Operating System (${siteConfig.title})`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
