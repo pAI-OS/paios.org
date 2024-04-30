bc55332b75fa9991c2f28a12e7ebeb27b16538fc
Sam Johnston <samj@samj.net>
2024-04-30 15:06:04 -0700

    fix blue background

--- src/components/BackgroundPattern/index.tsx
+++ src/components/BackgroundPattern/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function BackgroundPattern() {
   return (
-    <div className="absolute top-0 left-0 right-0 h-[300px] -z-10" aria-hidden>
-      <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent to-gray-50 dark:to-blue-600" />
+    <div className="absolute top-0 left-0 right-0 h-[350px] -z-10" aria-hidden>
+      <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent to-blue-700 dark:to-gray-500" />
       <div className={`bg-background-pattern w-full h-full opacity-10`} />
     </div>
   );
